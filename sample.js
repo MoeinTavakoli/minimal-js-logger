6f911fb1f82db307e8a1c79949474ca5c8a954ee
moeinTavakoli <mtawakoli2283@gmail.com>
2023-05-21 14:35:04 +0330

    [ Feature ] add sample usage as logger

---
+++ sample.js
@@ -0,0 +1,17 @@
+// import library 
+const Logger = require('minimal-js-logger')
+
+// create instance from this and pass the directory to save the log files.
+const loggerInstance = new Logger({ path: "./logs" })
+
+// sample log with notice severity
+loggerInstance.write('Logger started !', 'notice')
+
+// sample log with info severity
+loggerInstance.write('req.body : {username: "moein" , route : "/api/v1/information" }', 'info')
+
+// sample log with error severity
+loggerInstance.write('error : {message:"table in database not found"}', 'error')
+
+// sample log with critical severity
+loggerInstance.write('error : {message:"service postgres is not active"}', 'critical')
